f3dccd61278dea41210496d1627a107d8bc628e2
Yoe <ana-raquel@gmail.com>
2019-09-11 21:38:13 -0300

    Exercicio2-3

---
+++ exercicio2-3.js
@@ -0,0 +1,2 @@
+var salario = 3500.56;
+console.log('Salário informado é de: R$ :' + salario);
---
+++ exercicio2-3.ts
@@ -0,0 +1,6 @@
+let salario : number = 3500.56;
+console.log('Salário informado é de: R$ :' + salario );
+
+
+
+
