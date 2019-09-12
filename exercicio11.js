c88310d637876a2681c9f210f60f7654a7e59899
Yoe <ana-raquel@gmail.com>
2019-09-11 22:27:37 -0300

    exercicio11

---
+++ exercicio11.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+var exercicio10_1 = require("./exercicio10");
+var cuboNum = 4;
+console.log("o cubo do numero \u00E9 : " + exercicio10_1.cubo(cuboNum));
---
+++ exercicio11.ts
@@ -0,0 +1,4 @@
+import { cubo } from "./exercicio10";
+
+let cuboNum:number = 4;
+console.log(`o cubo do numero é : ${cubo(cuboNum)}`)
