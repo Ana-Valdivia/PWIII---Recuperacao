99c6a8664a5d75d401713ce7ad2d72b156258f60
Yoe <ana-raquel@gmail.com>
2019-09-11 22:19:20 -0300

    Exercicio9

---
+++ exercicio9.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+var exercicio8_1 = require("./exercicio8");
+var custo = new exercicio8_1.piscina(6, 75);
+console.log(custo.custoTotal());
---
+++ exercicio9.ts
@@ -0,0 +1,4 @@
+    import { piscina } from "./exercicio8";
+
+    let custo = new piscina(6,75);
+    console.log(custo.custoTotal());
