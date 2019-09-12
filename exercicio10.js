98ea7acb42c73bed4b85b84ec7462db2fc85c982
Yoe <ana-raquel@gmail.com>
2019-09-11 22:22:48 -0300

    Exercicio10

---
+++ exercicio10.js
@@ -0,0 +1,17 @@
+"use strict";
+exports.__esModule = true;
+//funcão exercicio 5
+exports.parImpar = function (numero) {
+    if (numero % 2 == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+};
+//funçao exercicio 6
+exports.calcArea = function (comprimento, largura) {
+    return (comprimento * largura);
+};
+//funcão exercicio 7
+exports.cubo = function (cuboNum) { return (cuboNum * cuboNum * cuboNum); };
---
+++ exercicio10.ts
@@ -0,0 +1,27 @@
+//funcão exercicio 5
+export let parImpar = function(numero:number):boolean
+    {
+        if(numero % 2 == 0)
+    {
+        return true
+    }
+    else
+    {
+       return false
+    }
+}
+
+
+
+
+
+//funçao exercicio 6
+export let calcArea = function(comprimento:number, largura:number):number
+{
+    return(comprimento * largura)
+}
+
+//funcão exercicio 7
+
+
+export let cubo = (cuboNum:number):number => (cuboNum * cuboNum * cuboNum)
